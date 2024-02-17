289b2d015f341692c60d8772fff8ce5090465033
Facundo Bautista Barbera <facundo.barbera@gmail.com>
2024-02-17 17:07:08 -0600

    A good commit

--- local_dependencies/repo_downloader.lua
+++ local_dependencies/repo_downloader.lua
@@ -1,30 +1,67 @@
 -- This is a library for downloading repositories from GitHub.
-local http = require("socket.http")
-local https = require("ssl.https")
-local lfs = require("lfs")
+local githubDownloader = {}
 
--- Function to download a file
-function downloadFile(url, path)
-    local body, code = https.request(url)
-    if code ~= 200 then
-        error("Failed to download file: " .. code)
+local httpAPIEnabled = http and true or false
+local defaultBranch = "main" -- Adjust based on common usage, "master" or "main"
+
+-- Check if HTTP API is enabled
+function githubDownloader.checkHTTPAPI()
+    return httpAPIEnabled
+end
+
+-- Download File
+function githubDownloader.downloadFile(path, url, name)
+    if not httpAPIEnabled then
+        error("HTTP API is disabled.")
     end
-
-    local file = assert(io.open(path, "wb"))
-    file:write(body)
-    file:close()
+    local dirPath = path:match('([%w%_%.% %-%+%,%;%:%*%#%=%/]+)/' .. name .. '$')
+    if dirPath ~= nil and not fs.isDir(dirPath) then
+        fs.makeDir(dirPath)
+    end
+    local content = http.get(url)
+    if content then
+        local file = fs.open(path, "w")
+        file.write(content.readAll())
+        file.close()
+    else
+        error("Failed to download file: " .. name)
+    end
 end
 
--- Function to download a GitHub repository
-function downloadRepo(user, repo, branch)
-    local url = string.format("https://github.com/%s/%s/archive/refs/heads/%s.zip", user, repo, branch)
-    local path = string.format("%s-%s.zip", repo)
-
-    -- Download the zip file
-    downloadFile(url, path)
-
-    -- TODO: Extract the zip file
-
-    -- Delete the zip file
-    lfs.remove(path)
+-- Get Directory Contents
+function githubDownloader.getGithubContents(user, repo, path, branch)
+    if not httpAPIEnabled then
+        error("HTTP API is disabled.")
+    end
+    local url = "https://api.github.com/repos/" .. user .. "/" .. repo .. "/contents/" .. (path or "") .. "?ref=" .. (branch or defaultBranch)
+    local response = http.get(url)
+    if response then
+        local pType, pPath, pName = {}, {}, {}
+        local result = response.readAll()
+        for str in result:gmatch('"type":"(%w+)"') do table.insert(pType, str) end
+        for str in result:gmatch('"path":"([^\"]+)"') do table.insert(pPath, str) end
+        for str in result:gmatch('"name":"([^\"]+)"') do table.insert(pName, str) end
+        return pType, pPath, pName
+    else
+        error("Error: Can't resolve URL " .. url)
+    end
 end
+
+-- Download Repository Contents
+function githubDownloader.downloadRepository(user, repo, path, branch, localPath)
+    local effectivePath = path or ""
+    local effectiveBranch = branch or defaultBranch
+    local effectiveLocalPath = localPath or "downloads/" .. repo
+    local pType, pPath, pName = githubDownloader.getGithubContents(user, repo, effectivePath, effectiveBranch)
+    for i, type in ipairs(pType) do
+        if type == "file" then
+            local filePath = effectiveLocalPath .. "/" .. pName[i]
+            local fileURL = "https://raw.githubusercontent.com/" .. user .. "/" .. repo .. "/" .. effectiveBranch .. "/" .. pPath[i]
+            githubDownloader.downloadFile(filePath, fileURL, pName[i])
+        elseif type == "dir" then
+            githubDownloader.downloadRepository(user, repo, pPath[i], effectiveBranch, effectiveLocalPath .. "/" .. pName[i])
+        end
+    end
+end
+
+return githubDownloader
