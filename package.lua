cdb204e8e835f7e873676f17252641a68c9e1670
Facundo Bautista Barbera <facundo.barbera@gmail.com>
2024-02-17 17:25:04 -0600

    Added package.lua
    This file contains information about the package, such as the name, version, and dependencies.

---
+++ package.lua
@@ -0,0 +1,14 @@
+return {
+    name = "lucratecc",
+    version = "0.1.0",
+    description = "A package manager for ComputerCraft",
+    authors = {
+        "BiXKu"
+    },
+    dependencies = {},
+    files = {
+        include = {},
+        exclude = {}
+    },
+    additional = {}
+}
